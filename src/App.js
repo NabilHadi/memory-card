c468386f013ab434745cf582caafc7665ce2bafe
Nabil Hadi <napilfadl@gmail.com>
2022-04-24 04:28:44 +0300

    add score to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Score from "./components/Score";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Memory Card</h1>
+      <div className="score-container">
+        <Score text="Current Score" score={0} />
+        <Score text="Best Score" score={0} />
+      </div>
     </div>
   );
 }
