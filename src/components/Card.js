dc38bfa85e3e347cc233e35ca0de1274ee431255
Nabil Hadi <napilfadl@gmail.com>
2022-04-25 22:20:54 +0300

    change card style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 export default function Card({ id, name, img, handleOnClick }) {
   return (
     <div
-      className=" flex flex-col w-52 bg-orange-100 p-4 text-center gap-2 border-orange-200 rounded-lg shadow-md hover:opacity-80 hover:shadow-lg active:opacity-100 justify-self-center"
+      className=" flex flex-col w-52 bg-orange-100 p-4 text-center gap-2 border-orange-200 rounded-lg shadow-md hover:opacity-90 hover:shadow-lg active:opacity-100 justify-self-center cursor-pointer hover:p-3"
       onClickCapture={() => handleOnClick(id)}
     >
       {/* <img src={img} alt="card" className=" rounded-lg" /> */}
